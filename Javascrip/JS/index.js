e72e1b54474141971490fdf74ca2963f1fd8f876
MinhThuong8694 <minhthuong8694@gmail.com>
2020-05-15 10:26:21 +0700

    them

---
+++ Javascrip/JS/index.js
@@ -0,0 +1,26 @@
+// console.log("Hello class")
+// alert("Hello world")
+var helloWorld ="Hello world! 2002e"
+let helloWorld2 = "Hello world"
+const helloWorld3 = "Hello world"
+// var, let, const đều định nghĩa 1 biến tuy nhiên dùng const không đc gán giá trị do const là hằng, và dùng const phải có hàm khởi tạo giá trị
+var number = 6;
+console.log(helloWorld, typeof(helloWorld))
+console.log(number, typeof(number))
+var x;
+var object = null
+console.log(x)
+console.log(object)
+//  caculate timer
+var space = 120 + 100 -60;
+var velocity = 20
+var timer = space/velocity;
+console.log(timer, "hours")
+console.log(9%5)
+var hello = "hello" + "" + "world"
+console.log(hello)
+
+// mix
+var mix = 5 - "hdsh5" 
+// có số cộng chuỗi ưu tiên chuỗi
+console.log("Mix: ", mix)
