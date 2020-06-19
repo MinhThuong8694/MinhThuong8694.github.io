efd3910635b498210d3e3a4cb38a3071f3afbcb9
MinhThuong8694 <minhthuong8694@gmail.com>
2020-06-19 23:48:55 +0700

    them

--- baitapjs/JS/bai19.js
+++ baitapjs/JS/bai19.js
@@ -1,2 +1,15 @@
 // 19.  Hãy tính tổng các số từ 1 đến n mà không dùng for, while loop? (đệ quy, recursive)
 // VD: total(9) => output: 45
+
+
+function recursive(n){
+
+    if(n = 0){
+        return 0;
+    }else{
+        return n + recursive(n-1);
+    }
+}
+
+console.log(recursive(5))
+
